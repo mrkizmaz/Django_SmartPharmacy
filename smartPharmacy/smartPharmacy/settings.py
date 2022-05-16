789bb066b478ef5813f4bcf63f75da88eef09751
Ersel Kızmaz <84200433+mrkizmaz@users.noreply.github.com>
2022-05-16 19:13:42 +0300

    Update settings.py

--- smartPharmacy/smartPharmacy/settings.py
+++ smartPharmacy/smartPharmacy/settings.py
@@ -153,6 +153,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_HOST_USER='smartphrmcy@gmail.com'
-EMAIL_HOST_PASSWORD='Pharmacysmart34'
+EMAIL_HOST_PASSWORD='*********'
 EMAIL_USE_TLS=True
 #EMAIL_USE_SSL=False
